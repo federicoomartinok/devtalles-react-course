26a42414e94d43bf6f8e2491c1ba4f600e493a94
Federico Martin <federicoomartinok@gmail.com>
2025-01-06 00:08:18 -0300

    Promises y fetch en js

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,15 +1,15 @@
 import heroes,{owners} from "../data/heroes";
 
 
-console.log(owners)
+// console.log(owners)
 
 
 // find devuelve solo el heroe que tiene el id que se le pasa
-const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
 
 //console.log(getHeroeById(2))
 
 //filter devuelve solo los que son de DC
-const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+export const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
 
-console.log(getHeroesByOwner('DC'))
+// console.log(getHeroesByOwner('DC'))
